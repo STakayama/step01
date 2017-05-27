ee58f546df4a0f1b25fb146ffdf91b1ab97f8416
高山沙也加 <sayaka@takayamasayaka-no-MacBook-Pro.local>
2017-05-27 18:08:18 +0900

    all

--- Step01.java
+++ Step01.java
@@ -5,54 +5,66 @@ import java.io.*;
 
 public class Step01{
 
-    void quicksort(char line,int left,int right){
-	if(left<right){
-	    char pivot;
+    static void quicksort(char[] line,int left,int right){
 	    int i=left;
 	    int j=right;
-	    int mid=(int)((i+(j-1))/2.0);
+	    char pivot=line[(int)((i+(j-1))/2.0)];
+	    System.out.println(pivot);
+
+	    while(i!=right-1||j!=0){System.out.println(i);System.out.println(j);
+		while(line[i]<pivot){
+		    i++;
+		}
+		while(line[j]>pivot){
+		    j--;
+		}
+		if(i>=j){
+		    break;
+		}
+
+		char temp=line[i];
+		line[i]=line[j];
+		line[j]=temp;
+	    }
 	    
-	    if(line[i]<line[j]){
-		if(line[j]<line[mid]){
-		    pivot=line[j]; 
-		}
-		else if(line[mid]<line[i]){
-		    pivot=line[i];
-		}
-		else{
-		    pivot-line[mid];
-		}
-
-		if(line){
-		}
-		else if(){
-		}
-		else(){
-		    }
-
-
-
+	    if(left<i-1){
+		quicksort(line,left,i-1);
+	    }
+	    if(right>j+1){
+		quicksort(line,j+1,right);
 	    }
 
 
+    
 
-
-
-	}
     }
 
     public static void main(String[] args){
 	//BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));
-
-	String input=args[0];
+	//	String input=args[0];　//入力受け取り
 	String[]  dict_line=new String[100000];
+	
+	File file=new File("../ex.txt");
+
 	try{
-	    FileReader dict=new FileReader("../dic.txt");
+	PrintWriter pw=new PrintWriter(new BufferedWriter(new FileWriter(file)));
+
+
+
+
+	try{
+	    FileReader dict=new FileReader("short.txt");
 	    BufferedReader d_buf=new BufferedReader(dict);
 	    int i=0;
 	    while(true){
-		char change[]=d_buf.readLine().toCharArray();//辞書の文字をどう入れ替えるか
+		char change[]=d_buf.readLine().toCharArray();//辞書の文字をどう入れ替えるか   一行の文字列をを一個一個格納
 		//	dict_line[i]
+		quicksort(change,0,change.length-1);
+
+		pw.println(change);
+		
+		System.out.println("f");
+
 		if(dict_line==null){
 		    break;
 		}	
@@ -63,7 +75,9 @@ public class Step01{
 	    System.out.println(e);
 	}
 	
+	}catch(IOException e){
 
+	}
 
 
 
