eb5ae528cc1345ed149081f62a60728bd2cd6445
高山沙也加 <sayaka@takayamasayaka-no-MacBook-Pro.local>
2017-05-27 16:07:32 +0900

    クイックソート(途中)

--- Step01.java
+++ Step01.java
@@ -5,6 +5,42 @@ import java.io.*;
 
 public class Step01{
 
+    void quicksort(char line,int left,int right){
+	if(left<right){
+	    char pivot;
+	    int i=left;
+	    int j=right;
+	    int mid=(int)((i+(j-1))/2.0);
+	    
+	    if(line[i]<line[j]){
+		if(line[j]<line[mid]){
+		    pivot=line[j]; 
+		}
+		else if(line[mid]<line[i]){
+		    pivot=line[i];
+		}
+		else{
+		    pivot-line[mid];
+		}
+
+		if(line){
+		}
+		else if(){
+		}
+		else(){
+		    }
+
+
+
+	    }
+
+
+
+
+
+	}
+    }
+
     public static void main(String[] args){
 	//BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));
 
@@ -15,7 +51,8 @@ public class Step01{
 	    BufferedReader d_buf=new BufferedReader(dict);
 	    int i=0;
 	    while(true){
-		dict_line[i]=d_buf.readLine();//辞書の文字をどう入れ替えるか
+		char change[]=d_buf.readLine().toCharArray();//辞書の文字をどう入れ替えるか
+		//	dict_line[i]
 		if(dict_line==null){
 		    break;
 		}	
